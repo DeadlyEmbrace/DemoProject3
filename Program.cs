83943e3dd36482c38b163a53a7a7f346121391d7
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2021-04-13 11:03:16 +0200

    Initial code
    - Created basic .Net project
    - Minor tweaks to template code

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DemoProject3
+{
+    public class Program
+    {
+        /// <summary>
+        /// Main entry point for the application
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
